3418cbbb825a31ab0932e494e04810d8d9161853
dhendrawirawan <119018562+dhendrawirawan@users.noreply.github.com>
2023-08-02 02:01:09 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,153 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Straight":return img`
+. . . . . f . . . . . 
+. . . . f f f . . . . 
+. . . f f f f f . . . 
+. . f f f f f f f . . 
+. f f f f f f f f f . 
+f f f f f f f f f f f 
+. . . . f f f . . . . 
+. . . . f f f . . . . 
+. . . . f f f . . . . 
+. . . . f f f . . . . 
+. . . . f f f . . . . 
+`;
+            case "image4":
+            case "Spin-7":return img`
+f f f f f f f . . . . . . . . . 
+f f f f f f . . . . . . . . . . 
+f f f f f . . . . . . . . . . . 
+f f f f f f . . . . . . . . . . 
+f f f . f f f . . . . . . . . . 
+f f . . . f f f . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . . f f . . . . . . . . 
+. . . . . f f f . . . . . . . . 
+. . . . . f f . . . . . . . . . 
+. . . . f f f . . . . . . . . . 
+. . . f f f . . . . . . . . . . 
+. . . f f . . . . . . . . . . . 
+`;
+            case "image5":
+            case "Spin-14":return img`
+fffffff..fffffff....
+ffffff...ffffff.....
+fffff....fffff......
+ffffff...ffffff.....
+fff.fff..fff.fff....
+ff...fff.ff...fff...
+......ff.......ff...
+......ff.......ff...
+......ff.......ff...
+......ff.......ff...
+......ff.......ff...
+.....fff......fff...
+.....ff.......ff....
+....fff......fff....
+...fff......fff.....
+...ff.......ff......
+`;
+            case "image6":
+            case "Spin-21":return img`
+fffffff..fffffff..fffffff.
+ffffff...ffffff...ffffff..
+fffff....fffff....fffff...
+ffffff...ffffff...ffffff..
+fff.fff..fff.fff..fff.fff.
+ff...fff.ff...fff.ff...fff
+......ff.......ff.......ff
+......ff.......ff.......ff
+......ff.......ff.......ff
+......ff.......ff.......ff
+......ff.......ff.......ff
+.....fff......fff......fff
+.....ff.......ff.......ff.
+....fff......fff......fff.
+...fff......fff......fff..
+...ff.......ff.......ff...
+`;
+            case "image7":
+            case "Spin21":return img`
+.fffffff..fffffff..fffffff
+..ffffff...ffffff...ffffff
+...fffff....fffff....fffff
+..ffffff...ffffff...ffffff
+.fff.fff..fff.fff..fff.fff
+fff...ff.fff...ff.fff...ff
+ff.......ff.......ff......
+ff.......ff.......ff......
+ff.......ff.......ff......
+ff.......ff.......ff......
+ff.......ff.......ff......
+fff......fff......fff.....
+.ff.......ff.......ff.....
+.fff......fff......fff....
+..fff......fff......fff...
+...ff.......ff.......ff...
+`;
+            case "image3":
+            case "Spin14":return img`
+.fffffff..fffffff...
+..ffffff...ffffff...
+...fffff....fffff...
+..ffffff...ffffff...
+.fff.fff..fff.fff...
+fff...ff.fff...ff...
+ff.......ff.........
+ff.......ff.........
+ff.......ff.........
+ff.......ff.........
+ff.......ff.........
+fff......fff........
+.ff.......ff........
+.fff......fff.......
+..fff......fff......
+...ff.......ff......
+`;
+            case "image2":
+            case "Spin7":return img`
+. f f f f f f f . . . . . . . . 
+. . f f f f f f . . . . . . . . 
+. . . f f f f f . . . . . . . . 
+. . f f f f f f . . . . . . . . 
+. f f f . f f f . . . . . . . . 
+f f f . . . f f . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f f f . . . . . . . . . . . . . 
+. f f . . . . . . . . . . . . . 
+. f f f . . . . . . . . . . . . 
+. . f f f . . . . . . . . . . . 
+. . . f f . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
